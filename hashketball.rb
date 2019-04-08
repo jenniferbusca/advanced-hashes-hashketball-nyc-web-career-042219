6cd9989036b0e319a28308c03df7818940838470
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-08 21:50:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -211,7 +211,7 @@ end
 def winning_team
   points_list = Hash.new(0)
   game_hash.each do |team|
-    points_list[team][:team_name] += player_data[:points]
+    points_list[:team_name] += player_data[:points]
   end
   return points_list.key(points_list.values.max)
 end
